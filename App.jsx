d3efcdf94bf2b13399f6199fbacceade9b96b1b3
tanvikurhade <83345643+tanvikurhade@users.noreply.github.com>
2021-06-27 13:47:01 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function List() {
+  return (a
+    <ul>
+      <li>First</li>
+      <li>Second</li>
+      <li>Third</li>
+    </ul>
+  );
+}
+
+export default List;
